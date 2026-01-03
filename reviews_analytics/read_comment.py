de8ddd62fc1909ba049f94246a86ca9f9a77cbe6
kuojiajie <kuojiajie9999@gmail.com>
2026-01-04 00:03:14 +0800

    comment_avg_word

--- reviews_analytics/read_comment.py
+++ reviews_analytics/read_comment.py
@@ -5,8 +5,13 @@ with open('reviews.txt', 'r') as file:
         data.append(line)
         if count % 1000 == 0:
             print(len(data))
-print('讀取完畢，一共有', count, '則留言')
+print('讀取完畢，一共有', len(data), '則留言')
 
 print('第1則留言: ', data[0])
 print('第2則留言: ', data[1])
 print('第666666則留言: ', data[666665])
+
+sum_len = 0
+for comment in data:
+    sum_len += len(comment)
+print('每則留言平均有', sum_len / len(data), '字')
